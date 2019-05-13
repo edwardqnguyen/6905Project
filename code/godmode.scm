1e3e3a94157d2479895aba2d2a4ade85b4001e17
edwardqnguyen <edwardnguyen101@gmail.com>
2019-05-13 19:19:31 -0400

    Fixed unspecific list bug in godmode

--- code/godmode.scm
+++ code/godmode.scm
@@ -4,21 +4,13 @@
 (define (enable-god-mode) (set! god-mode-enabled #t))
 (define (disable-god-mode) (set! god-mode-enabled #f))
 
-(define (function-available? func)
-	(hash-table/lookup god-mode-exclusive func
-			   (lambda (key) ((or (eq? (hash-table-ref key) #f)
-					      (eq? god-mode-enabled #t))))
-			   (lambda () (#f))))
-
 (define (god-mode-only func)
-	(lambda arguments (
-		(if (eq? god-mode-enabled #f)
+	(lambda arguments
+        (if (eq? god-mode-enabled #f)
 		    (narrate! (list "God-mode Disabled")
                   my-avatar)
 		    (apply func arguments))
-	)))
-
-(define god-mode-test (god-mode-only (lambda () (display "its working"))))
+	))
 
 (define (god-go direction)
 	((god-mode-only go) direction))
