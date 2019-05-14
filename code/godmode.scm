46ececa9474417cd5709d11a6d6978187cef6dd7
edwardqnguyen <edwardnguyen101@gmail.com>
2019-05-14 01:00:39 -0400

    added structure for eval/apply

--- code/godmode.scm
+++ code/godmode.scm
@@ -4,13 +4,20 @@
 (define (enable-god-mode) (set! god-mode-enabled #t))
 (define (disable-god-mode) (set! god-mode-enabled #f))
 
+(define (function-available? func) ; Unused Sticky Note Checker for interpreter
+	(hash-table/lookup god-mode-exclusive func
+			   (lambda (key) ((or (eq? (hash-table-ref key) #f)
+					      (eq? god-mode-enabled #t))))
+			   (lambda () (#f))))
+
 (define (god-mode-only func)
 	(lambda arguments
         (if (eq? god-mode-enabled #f)
 		    (narrate! (list "God-mode Disabled")
                   my-avatar)
-		    (apply func arguments))
-	))
+		    (apply func arguments))))
+
+;;;;; God-Mode Wrapped Functions
 
 (define (god-go direction)
 	((god-mode-only go) direction))
